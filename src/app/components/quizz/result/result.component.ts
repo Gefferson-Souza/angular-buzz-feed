774b38a9a569d9c28490d01bdb8d493f9400fc66
Gefferson-Souza <geffersonteodorodesouza@gmail.com>
2023-11-15 16:54:12 -0300

    add twitter share

--- src/app/components/quizz/result/result.component.ts
+++ src/app/components/quizz/result/result.component.ts
@@ -26,8 +26,7 @@ export class ResultComponent implements OnInit {
   }
 
   shareOnSocialMedia() {
-    const message = `Eu sou DEV do tipo: \n ${this.description} \n  ${this.professions.map(p => p.name).join('\n')} \n
-
+    const message = `Eu sou DEV do tipo:\n${this.description}\n${this.professions.map(p => p.name).join('\n')}\nQual é o seu Tipo Dev? acesse o link abaixo e descruba!\nhttps://seutipodev.netlify.app/
     `;
 
     const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}`;
